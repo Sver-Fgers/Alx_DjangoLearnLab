8812dbc9bd98fb5754fd75f3cc8cbb0c90ac0204
Victor Nanmwa <Victor_nxx@protonmail.com>
2025-07-20 22:53:05 +0100

    chore: remove django-model project

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')   # show these in the admin list
-    search_fields = ('title', 'author')                      # search by title or author
-    list_filter = ('publication_year',)                      # filter by publication year
